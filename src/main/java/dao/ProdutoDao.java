0f6a5b5796dedc943a45dc01f64432cd0acbf331
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2022-12-03 21:52:21 -0300

    feat: add queries

--- src/main/java/dao/ProdutoDao.java
+++ src/main/java/dao/ProdutoDao.java
@@ -5,6 +5,7 @@ import loja.modelo.Produto;
 import util.JPAutil;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ProdutoDao {
@@ -18,11 +19,12 @@ public class ProdutoDao {
         this.em.persist(produto);
     }
 
-    public void atualizar(Produto produto){
+    public void atualizar(Produto produto) {
         this.em.merge(produto);
     }
+
     public void remover(Produto produto) {
-        produto =  em.merge(produto);
+        produto = em.merge(produto);
         this.em.remove(produto);
     }
 
@@ -35,5 +37,26 @@ public class ProdutoDao {
         return em.createQuery(jpql, Produto.class).getResultList();
     }
 
+    public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
+    }
 }
+
 
--- src/main/java/loja/modelo/CadastroProduto.java
+++ src/main/java/loja/modelo/CadastroProduto.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroProduto {
 
@@ -18,10 +19,26 @@ public class CadastroProduto {
         ProdutoDao produtoDao = new ProdutoDao(em);
         Produto p = produtoDao.buscarPorId(1l);
         System.out.println(p.getPreco());
+        List<Produto> todos = produtoDao.buscarTodos();
+        List<Produto> produtoPorCategoria = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+
+        todos.forEach(prod -> System.out.println("PRODUTO: "+prod.getNome()));
+
+        produtoPorCategoria.forEach(prod2 -> System.out.println("PRODUTO POR CATEGORIA: "+prod2.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi 5S plus");
+        System.out.println("PRECO DO PRODUTO"+ precoDoProduto);
+
+
     }
     private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
+        Categoria eletrodomesticos = new Categoria("ELETRODOMESTICOS");
+
         Produto celular = new Produto("Xiaomi Redmi 5S plus","Muito rapido e duradouro",new BigDecimal("800"), celulares);
+        Produto geladeira = new Produto("Geladeira Consul","Muito eficiente e economica",new BigDecimal("1200"), eletrodomesticos);
+        Produto novaGeladeira = new Produto("Geladeira Brastemp Inverse Smart Bar","Top de linha do mercado, melhor modelo da marca",new BigDecimal("3100"), eletrodomesticos);
+
 
         EntityManager em = JPAutil.getEntityManager();
 
@@ -32,6 +49,11 @@ public class CadastroProduto {
 
         categoriaDao.cadastrar(celulares);
         produtoDao.cadastrar(celular);
+
+        categoriaDao.cadastrar(eletrodomesticos);
+        produtoDao.cadastrar(geladeira);
+        produtoDao.atualizar(novaGeladeira);
+        produtoDao.remover(geladeira);
 
         em.getTransaction().commit();
         em.close();
